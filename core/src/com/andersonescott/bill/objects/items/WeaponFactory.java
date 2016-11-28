2d5f674650626b2a6f494e1fb1ca711baee3c8ec
cescott <chuckescott@comcast.net>
2016-11-28 09:10:20 -0800

    still workin on wepoin

--- core/src/com/andersonescott/bill/objects/items/WeaponFactory.java
+++ core/src/com/andersonescott/bill/objects/items/WeaponFactory.java
@@ -31,6 +31,13 @@ public class WeaponFactory {
     protected static ArrayList<String> staffNames;
     protected static ArrayList<String> hammerNames;
 
+    protected String[] effects = {
+            "Power",
+            "Mana",
+            "Health",
+            "HealthRep"
+    };
+
     public WeaponFactory() {
         for (int i = 0; i < sword.length; i++) {
             swordNames.add(sword[i]);
@@ -48,6 +55,8 @@ public class WeaponFactory {
 
     public Weapon makeWeapon(int playerLvl){
         int level;
+        String type;
+        String name;
         int rare = (int)(Math.random() * 100);
         Item.Rarity rarity;
 
@@ -72,7 +81,55 @@ public class WeaponFactory {
             rarity = Item.Rarity.COMMON;
         }
 
+        int dmgInt = (int)(Math.random()*80) + 20;
+        int critInt = (int)(Math.random()*80) + 20;
+        int manaRepInt = (int)(Math.random()*80) + 20;
 
+        double dmgPercent = dmgInt/(dmgInt + critInt + manaRepInt);
+        double critPercent = critInt/(dmgInt + critInt + manaRepInt);
+        double manaRepPercent = manaRepInt/(dmgInt + critInt + manaRepInt);
+
+        if (dmgPercent >= .5) {
+            type = "Hammer";
+            name = hammerNames.remove((int)(Math.random()*hammerNames.size())) + "hammer";
+            if (hammerNames.size() == 0) {
+                for (int i = 0; i < hammer.length; i++) {
+                    hammerNames.add(hammer[i]);
+                }
+            }
+        }
+        else if (critPercent >= .5) {
+            type = "Dagger";
+            name = daggerNames.remove((int)(Math.random()*daggerNames.size())) + "dagger";
+            if (daggerNames.size() == 0) {
+                for (int i = 0; i < dagger.length; i++) {
+                    daggerNames.add(dagger[i]);
+                }
+            }
+        }
+        else if (manaRepPercent >= .5) {
+            type = "Staff";
+            name = staffNames.remove((int)(Math.random()*staffNames.size())) + "staff";
+            if (staffNames.size() == 0) {
+                for (int i = 0; i < staff.length; i++) {
+                    staffNames.add(staff[i]);
+                }
+            }
+        }
+        else {
+            type = "Sword";
+            name = swordNames.remove((int)(Math.random()*swordNames.size())) + "sword";
+            if (swordNames.size() == 0) {
+                for (int i = 0; i < sword.length; i++) {
+                    swordNames.add(sword[i]);
+                }
+            }
+        }
+
+        String effect = effects[(int)(Math.random()*4)];
+
+        if (effect.equals("Power")) {
+            
 
         return new Weapon(level, rarity, image, );
     }
