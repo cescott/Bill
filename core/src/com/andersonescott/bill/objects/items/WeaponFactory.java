78f3068771996037a87974cd557f0668f0ac4c4e
cescott <chuckescott@comcast.net>
2016-12-05 08:41:07 -0800

    yeah i dunnit
    congration

--- core/src/com/andersonescott/bill/objects/items/WeaponFactory.java
+++ core/src/com/andersonescott/bill/objects/items/WeaponFactory.java
@@ -6,6 +6,22 @@ import java.util.ArrayList;
 
 public class WeaponFactory {
 
+    /*
+    -
+    -
+    -
+    -
+    All weapon types should be capitalized,
+    or you could like
+    fix it
+    or something
+    idc
+    -
+    -
+    -
+    -
+     */
+
     protected String[] sword = {
             "Swanky",
             "S-s-s-",
@@ -19,7 +35,7 @@ public class WeaponFactory {
             "Purple",
             "Sonofa",
             "Sparkle",
-
+            "Radish",
     };
     protected String[] dagger = {
             "D-d-d-",
@@ -28,7 +44,7 @@ public class WeaponFactory {
             "Dingle",
             "Dongle",
             "Didgeri",
-            
+            "Dad-",
     };
     protected String[] staff = {
             "Swanky",
@@ -42,14 +58,12 @@ public class WeaponFactory {
             "Sonofa",
             "Sparkle",
             "Staff",
-
     };
     protected String[] hammer = {
             "Hella",
             "Hhhh",
             "Hammer",
             "Purple",
-
     };
     protected static ArrayList<String> swordNames;
     protected static ArrayList<String> daggerNames;
@@ -77,6 +91,124 @@ public class WeaponFactory {
         }
         for (int i = 0; i < hammer.length; i++) {
             hammerNames.add(hammer[i]);
+        }
+    }
+
+    public Weapon makeWeapon(int playerLvl, String weaponType) {
+        while (true) {
+        int level;
+        String type;
+        String name;
+
+        int dmgInt = (int)(Math.random()*80) + 20;
+        int critInt = (int)(Math.random()*80) + 20;
+        int manaRepInt = (int)(Math.random()*80) + 20;
+
+        double dmgPercent = dmgInt/(dmgInt + critInt + manaRepInt);
+        double critPercent = critInt/(dmgInt + critInt + manaRepInt);
+        double manaRepPercent = manaRepInt/(dmgInt + critInt + manaRepInt);
+
+        if (dmgPercent >= .5) {
+            type = "Hammer";
+            if (!weaponType.equals("Hammer")) {
+                continue;
+            }
+                name = hammerNames.remove((int) (Math.random() * hammerNames.size())) + "hammer";
+                if (hammerNames.size() == 0) {
+                    for (int i = 0; i < hammer.length; i++) {
+                        hammerNames.add(hammer[i]);
+                    }
+                }
+            }
+        else if (critPercent >= .5) {
+            if (!weaponType.equals("Dagger")) {
+                continue;
+            }
+                type = "Dagger";
+                name = daggerNames.remove((int) (Math.random() * daggerNames.size())) + "dagger";
+                if (daggerNames.size() == 0) {
+                    for (int i = 0; i < dagger.length; i++) {
+                        daggerNames.add(dagger[i]);
+                    }
+                }
+            }
+        else if (manaRepPercent >= .5) {
+            if (!weaponType.equals("Staff")) {
+                continue;
+            }
+                type = "Staff";
+                name = staffNames.remove((int) (Math.random() * staffNames.size())) + "staff";
+                if (staffNames.size() == 0) {
+                    for (int i = 0; i < staff.length; i++) {
+                        staffNames.add(staff[i]);
+                    }
+                }
+            }
+        else {
+            if (!weaponType.equals("Sword") && (weaponType.equals("Staff") || weaponType.equals("Hammer") || weaponType.equals("Dagger"))) {
+                continue;
+            }
+            type = "Sword";
+            name = swordNames.remove((int) (Math.random() * swordNames.size())) + "sword";
+            if (swordNames.size() == 0) {
+                for (int i = 0; i < sword.length; i++) {
+                    swordNames.add(sword[i]);
+                }
+            }
+        }
+
+            int bonusHealth = 0;
+            int healthRep = 0;
+            int bonusMana = 0;
+            int rare = (int) (Math.random() * 100);
+            Item.Rarity rarity;
+            double rarityScale = 1;
+
+            if (playerLvl > 2) {
+                level = playerLvl + (int) (Math.random() * 5) - 2;
+            } else {
+                level = playerLvl;
+            }
+
+            double levelScale = Math.pow(1.1, (level - 1));
+
+            if (rare >= 98) {
+                rarity = Item.Rarity.LEGENDARY;
+                rarityScale = 1.6;
+            } else if (rare >= 85) {
+                rarity = Item.Rarity.RARE;
+                rarityScale = 1.25;
+            } else if (rare >= 50) {
+                rarity = Item.Rarity.UNCOMMON;
+                rarityScale = 1.1;
+            } else {
+                rarity = Item.Rarity.COMMON;
+                rarityScale = 1;
+            }
+
+            String effect = effects[(int) (Math.random() * effects.length)];
+
+            if (effect.equals("Power")) {
+                dmgPercent += .1;
+                name = "Extra pokey " + name;
+            } else if (effect.equals("Mana")) {
+                bonusMana = (int) (30 * levelScale * rarityScale);
+                name = "Sparkly " + name;
+            } else if (effect.equals("Health")) {
+                bonusHealth = (int) (15 * levelScale * rarityScale);
+                name = name + " of Gains";
+            } else if (effect.equals("HealthRep")) {
+                healthRep = (int) (2.5 * levelScale * rarityScale);
+                name = "Overpowered " + name;
+            }
+
+            int damage = (int) (dmgPercent * rarityScale * levelScale * 30);
+            int critChance = (int) (critPercent * 100) - 9;
+            int manaRep = (int) (manaRepPercent * rarityScale * levelScale * 30);
+
+            Texture image = new Texture("badlogic.jpg");
+
+            return new Weapon(level, rarity, name, type, image, damage, manaRep, critChance, bonusHealth, healthRep, bonusMana);
         }
     }
 
@@ -182,7 +314,7 @@ public class WeaponFactory {
         }
 
         int damage = (int)(dmgPercent*rarityScale*levelScale*30);
-        int critChance = (int)(critPercent*100);
+        int critChance = (int)(critPercent*100) - 9;
         int manaRep = (int)(manaRepPercent*rarityScale*levelScale*30);
 
         Texture image =  new Texture("badlogic.jpg");
